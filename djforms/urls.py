5096ea64b93b1ee0ebe6c0de95f9a0444b54b220
markus1991kartal <markus1991kartal@gmail.com>
2020-02-25 18:25:02 +0200

    made basic app with template

--- djforms/urls.py
+++ djforms/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('question/', include('question.urls')), #почитать о функции include я не совсем понимаю
+                                                #я не совсем понимаю второй аргументы функции
 ]
--- question/views.py
+++ question/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Question
+
+
+def index(request):
+    questions = Question.objects.all()
+    return render(request, 'question/index.html', {"questions": questions})
